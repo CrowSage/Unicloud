231e31e15612e6d3569d95e145fc806d8dd99b91
Waliimran <waliimran.dev@gmail.com>
2026-07-10 16:12:38 +0500

    Added: Dropbox Connection

--- services/urls.py
+++ services/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("google/connect/", views.google_connect),
     path("google/callback/", views.google_callback),
     path("google/files/", views.google_files),
+    path("dropbox/connect/", views.dropbox_connect),
+    path("dropbox/callback/", views.dropbox_callback),
 ]
--- services/views.py
+++ services/views.py
@@ -3,7 +3,7 @@ from google_auth_oauthlib.flow import Flow
 from django.shortcuts import redirect
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from google.oauth2 import id_token
 from google.auth.transport import requests as google_requests
 from .models import ConnectedService
@@ -13,6 +13,10 @@ import secrets
 from django.utils import timezone
 import requests
 from .utils import validate_token
+from urllib.parse import urlencode
+import hashlib
+import base64
+from datetime import timedelta
 
 
 # Views
@@ -106,13 +110,15 @@ def google_callback(request):
 
     account_email = id_info["email"]
     print(credentials.expiry.tzinfo)
-    ConnectedService.objects.create(
+    ConnectedService.objects.update_or_create(
+        user=user,
         name="google",
         account_email=account_email,
-        access_token=access_token,
-        access_expiry=timezone.make_aware(credentials.expiry),
-        refresh_token=refresh_token,
-        user=user,
+        defaults={
+            "access_token": access_token,
+            "access_expiry": timezone.make_aware(credentials.expiry),
+            "refresh_token": refresh_token,
+        },
     )
 
     return Response(
@@ -140,3 +146,96 @@ def google_files(request):
     )
 
     return Response(response.json())
+
+
+# DROPBOX CONNECTION
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def dropbox_connect(request):
+
+    redirect_uri = "http://localhost:8000/api/services/dropbox/callback/"
+
+    code_verifier = secrets.token_urlsafe(64)
+    signed_state = signing.dumps(
+        {
+            "user_id": request.user.id,
+            "code_verifier": code_verifier,
+        }
+    )
+
+    code_challenge = (
+        base64.urlsafe_b64encode(hashlib.sha256(code_verifier.encode("utf-8")).digest())
+        .decode("utf-8")
+        .rstrip("=")
+    )
+
+    params = {
+        "client_id": settings.DROPBOX_CLIENT_ID,
+        "redirect_uri": redirect_uri,
+        "response_type": "code",
+        "token_access_type": "offline",
+        "code_challenge": code_challenge,
+        "code_challenge_method": "S256",
+        "state": signed_state,
+    }
+
+    auth_url = f"https://www.dropbox.com/oauth2/authorize?{urlencode(params)}"
+    return Response({"auth_url": auth_url})
+
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def dropbox_callback(request):
+
+    code = request.GET.get("code")
+    state = request.GET.get("state")
+
+    try:
+        data = signing.loads(state, max_age=300)
+    except signing.BadSignature:
+        return Response({"error": "Invalid or tampered state"}, status=400)
+
+    code_verifier = data["code_verifier"]
+    user_id = data["user_id"]
+
+    url = "https://api.dropboxapi.com/oauth2/token"
+    data = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "code_verifier": code_verifier,
+        "client_id": settings.DROPBOX_CLIENT_ID,
+        "client_secret": settings.DROPBOX_CLIENT_SECRET,
+        "redirect_uri": "http://localhost:8000/api/services/dropbox/callback/",
+    }
+
+    response = requests.post(url, data=data)
+
+    if response.status_code != 200:
+        return Response({"error": "Server Error"}, status=400)
+
+    data = response.json()
+
+    try:
+        user = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return Response({"error": "User Not Found"}, status=404)
+
+    access_token = data["access_token"]
+    refresh_token = data["refresh_token"]
+    account_id = data["account_id"]
+    access_expiry = timezone.now() + timedelta(seconds=data["expires_in"])
+
+    ConnectedService.objects.update_or_create(
+        user=user,
+        name="google",
+        account_email=account_id,
+        defaults={
+            "access_token": access_token,
+            "access_expiry": access_expiry,
+            "refresh_token": refresh_token,
+        },
+    )
+
+    return Response(
+        {"message": "Dropbox account connected successfully", "account_id": account_id}
+    )
