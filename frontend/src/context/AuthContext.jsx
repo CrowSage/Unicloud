fcc05c6f94f2dccdbbcab250df884e759f3b15a8
Waliimran <waliimran.dev@gmail.com>
2026-07-15 18:25:15 +0500

    Added: Api Request Function

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useState } from "react";
 import { API_URL } from "../config";
+import { configs } from "eslint-plugin-react-hooks";
 
 export const AuthContext = createContext()
 
@@ -38,7 +39,7 @@ export function AuthProvider({ children }) {
     // LOGIN FUNCTION
     async function login(username, password) {
 
-        const response = await fetch(`${API_URL}/accounts/login/`, {
+        const response = await fetch(`${API_URL}/accounts/token/`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password })
@@ -72,9 +73,93 @@ export function AuthProvider({ children }) {
     }
 
 
+    // This Function Refresh AccessToken Using refreshToken otherwise logsout
+    async function tokenRefresher() {
+
+        const response = await fetch(`${API_URL}/accounts/token/refresh/`, {
+
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ "refresh": user.refreshToken })
+
+        })
+
+        if (response.ok) {
+
+            const data = await response.json()
+
+            const updatedUser = {
+                ...user,
+                accessToken: data.access,
+            }
+
+            setUser(updatedUser)
+            localStorage.setItem("userObject", JSON.stringify(updatedUser))
+
+            return {
+                success: true,
+                access: data.access
+            }
+        }
+
+        return {
+            success: false
+        }
+    }
+
+
+    // API_Request Function
+    async function apiRequest(endpoint, options = {}) {
+
+        const config = {
+            ...options,
+            headers: {
+                ...options.headers || {},
+                "Authorization": `Bearer ${user.accessToken}`
+            }
+        }
+
+        const response = await fetch(`${API_URL}/${endpoint}`, config)
+
+
+        // Return if access token was valid
+        if (response.status !== 401) {
+            return response
+        }
+
+        // Getting a new access token and retrying otherwise
+        const refreshed = await tokenRefresher()
+
+
+
+        if (!refreshed.success) {
+            logout()
+            return
+        }
+
+        const retryConfig = {
+            ...options,
+            headers: {
+                ...options.headers || {},
+                "Authorization": `Bearer ${refreshed.access}`
+            }
+        }
+
+        const newResponse = await fetch(`${API_URL}/${endpoint}`, retryConfig)
+
+        if (newResponse.status === 401) {
+
+            logout()
+            return
+
+        }
+
+        return newResponse
+    }
+
 
     return (
-        <AuthContext.Provider value={{ register, login, logout, user }}>
+        <AuthContext.Provider value={{ register, login, logout, user, apiRequest }}>
             {children}
         </AuthContext.Provider>
     )
